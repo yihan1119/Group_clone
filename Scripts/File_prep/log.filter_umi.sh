9c72a97b5269a92a063c766c26e27beb88d08686
Yihan Wang <50383583+yihan1119@users.noreply.github.com>
2022-01-26 15:04:04 -0600

    Update log.filter_umi.sh

--- Scripts/File_prep/log.filter_umi.sh
+++ Scripts/File_prep/log.filter_umi.sh
@@ -1,12 +1,12 @@
 #!/bin/tcsh
 
-#SBATCH --job-name=filter_umi                     # job name
-#SBATCH --partition=super                   # select partion from 128GB, 256GB, 384GB, GPU and super
+#SBATCH --job-name=filter_umi                     	  # job name
+#SBATCH --partition=super                   		  # select partion from 128GB, 256GB, 384GB, GPU and super
 #SBATCH --nodes=1                                         # number of nodes requested by user
 #SBATCH --time=60-00:00:00                                # run time, format: D-H:M:S (max wallclock time)
 #SBATCH --output=serialJob.%j.out                         # standard output file name
 #SBATCH --error=serialJob.%j.time                         # standard error output file name
-#SBATCH --mail-user=Yihan.Wang@utsouthwestern.edu           # specify an email address
+#SBATCH --mail-user=User@email			          # specify an email address
 #SBATCH --mail-type=end                                   # send email when job status change (start, end, abortion and etc.)
 
 setenv PATH /home2/s426305/.conda/envs/py36/bin:$PATH
@@ -15,7 +15,7 @@ which python
 python --version
 
 set DATA_DIR=/project/GCRB/Hon_lab/s426305/Sequencing_data_analysis/10X/YW49-YW54_10x_mapping/nova_seq_data/sgRNA_lib_regex
-set SCRIPT_DIR=/project/GCRB/Hon_lab/s426305/Script/sgrna_regex
+
 
 foreach LIB(\
 		    YW61\
@@ -30,7 +30,7 @@ set NUM=`grep $LIB $DATA_DIR/lib_num.txt | cut -f2`
 echo $LIB
 echo $NUM
 
-$SCRIPT_DIR/process_regex_sgrna_df.py\
+./process_regex_sgrna_df.py\
     -i $DATA_DIR/$LIB.sgRNA_count.txt\
     -n $NUM\
     -o $DATA_DIR/$LIB.sgRNA_adj_df.pkl
