f6653622b869e98c9139593f8edb3ebca11f14d8
Yihan Wang <50383583+yihan1119@users.noreply.github.com>
2022-01-26 15:07:03 -0600

    Update and rename log_templet.sh to log_sgRNA_map.sh

--- Scripts/File_prep/log_sgRNA_map.sh
+++ Scripts/File_prep/log_sgRNA_map.sh
@@ -6,19 +6,18 @@
 #SBATCH --time=60-00:00:00                                # run time, format: D-H:M:S (max wallclock time)
 #SBATCH --output=serialJob.%j.out                         # standard output file name
 #SBATCH --error=serialJob.%j.time                         # standard error output file name
-#SBATCH --mail-user=Yihan.Wang@utsouthwestern.edu           # specify an email address
+#SBATCH --mail-user=User@email		                  # specify an email address
 #SBATCH --mail-type=end                                   # send email when job status change (start, end, abortion and etc.)
 
 setenv PATH ~/.conda/envs/py36/bin:$PATH
 
-set SCRIPT_DIR=/project/GCRB/Hon_lab/s160875/00.Scripts/10X_genomics/barcode_extraction
-set sgRNA_ref = /project/GCRB/Hon_lab/s426305/Sequencing_data_analysis/10X/YW49-YW54_10x_mapping/10x_sgRNA_RX_pipeline/YWsg1.txt
+set sgRNA_ref = ./YWsg1.txt
 set BARCODE_DIR=/project/GCRB/Hon_lab/s426305/Sequencing_data_analysis/10X/YW49-YW54_10x_mapping/nova_seq_data/10x_pipeline_transcriptome/10x_lib_10x_HTO_nova/outs/filtered_feature_bc_matrix
 set FASTQ_DIR=/project/GCRB/Hon_lab/s426305/Sequencing_data_analysis/10X/YW49-YW54_10x_mapping/nova_seq_data/sgRNA_lib_regex/SGRNA.tmp_fastq_combined
 
 zcat $BARCODE_DIR/barcodes.tsv.gz > 10x_lib.barcodes.tsv
 
-$SCRIPT_DIR/extract_sgRNA_from_reads.py\
+./extract_sgRNA_from_reads.py\
     -i $FASTQ_DIR/SGRNA_R1.fastq.gz $FASTQ_DIR/SGRNA_R2.fastq.gz\
     -b 10x_lib.barcodes.tsv\
     -r $sgRNA_ref\
@@ -27,7 +26,7 @@ $SCRIPT_DIR/extract_sgRNA_from_reads.py\
     -o SGRNA.sgRNA_cell-bc.summary.txt
 
 	
-$SCRIPT_DIR/summarize_sgRNA.ver2.py\
+./summarize_sgRNA.ver2.py\
     -i SGRNA.sgRNA_cell-bc.summary.txt\
     -o SGRNA.sgRNA_count.txt
 
